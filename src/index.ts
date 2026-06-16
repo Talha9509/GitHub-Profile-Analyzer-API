d9c2e449e0ff12c3ec46795a4fbba7683db66a1a
Mohd Talha <mawtalha5112004@gmail.com>
2026-06-16 16:44:59 +0530

    config: vercel deploy configs

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,5 @@ app.use((req,res,err) => {
   return res.status(500).json({ message: "Internal Server Error" })
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+export default app
